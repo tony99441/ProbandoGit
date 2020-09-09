42900513408b9761645701724c6cfea56e994fad
Anthony Briones Quiroz <n00017665@upn.pe>
2020-09-09 11:36:18 -0500

    Prueba Git

---
+++ ConceptosPoo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConceptosPoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            RealizarTarea();
+
+        }
+
+        static void RealizarTarea() {
+
+            Punto origen = new Punto();
+
+            Punto destino = new Punto(128,50);
+
+            double distancia = origen.MedirDistancia(destino);
+            Console.WriteLine("La distancia entre estos dos puntos es de: "+distancia);
+
+            Console.WriteLine("Numero de objetos creados: " + Punto.contanto());
+
+        }
+
+       
+
+
+    }
+}
---
+++ ConceptosPoo/Punto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ConceptosPoo
+{
+    class Punto
+    {
+        public Punto(int x, int y) {
+
+            this.x = x;
+            this.y = y;
+            contadorobjetos++;
+
+        }
+        public Punto() {
+
+            this.x = 0;
+            this.y = 0;
+            contadorobjetos++;
+        }
+
+        public double MedirDistancia(Punto otroPunto) {
+            int xdif = this.x - otroPunto.y;
+            int ydfi = this.y - otroPunto.y;
+
+            double distanciaPuntos = Math.Sqrt(Math.Pow(xdif, 2) + Math.Pow(ydfi, 2));
+            return distanciaPuntos;
+
+        }
+
+
+        public static int contanto() {
+            return contadorobjetos;
+        }
+
+        private int x, y;
+
+    private static int contadorobjetos = 0;
+
+}
+
+}
